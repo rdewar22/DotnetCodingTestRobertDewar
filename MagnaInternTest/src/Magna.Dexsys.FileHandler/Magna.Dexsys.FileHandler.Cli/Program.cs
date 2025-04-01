39b4caf66ca4e200c76d0110d64d819c5dabcc5d
Robert Dewar <108371693+rdewar22@users.noreply.github.com>
2025-04-01 13:54:07 -0400

    Uploading initial completion

---
+++ MagnaInternTest/src/Magna.Dexsys.FileHandler/Magna.Dexsys.FileHandler.Cli/Program.cs
@@ -0,0 +1,40 @@
+﻿using Magna.Dexsys.FileHandler.Models;
+using Magna.Dexsys.FileHandler.Services;
+using System.Diagnostics;
+
+namespace Magna.Dexsys.FileHandler.Cli;
+
+public class Program
+{
+    private const string _fileLocation = "C:\\temp\\generatedFiles";
+    private const string _searchValue = "OLD";
+
+    public static void Main(string[] args)
+    {
+        int testCount = 0;
+        int testLimit = 10;
+
+        for (; testCount < testLimit; testCount++)
+        {
+            Test(_searchValue);
+        }
+
+        Console.ReadLine();
+    }
+
+    private static void Test(string searchValue)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        FileSearchService searchService = new();
+        searchService.LocateFilesContainingSearchValue(_fileLocation, searchValue);
+        stopwatch.Stop();
+
+        foreach (FileDetails item in searchService.FilesLocated)
+        {
+            Console.WriteLine(item.Name, item.Content);
+        }
+
+        Console.WriteLine($"Completed in {stopwatch.ElapsedMilliseconds}");
+    }
+}
---
+++ MagnaInternTest/src/Magna.Dexsys.FileHandler/Magna.Dexsys.FileHandler/Models/FileDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magna.Dexsys.FileHandler.Models;
+public class FileDetails
+{
+    public string Path { get; set; }
+    public string Name { get; set; }
+    public int? ContentLength { get; set; }
+    public string? Content { get; set; }
+
+    public FileDetails(string path, string name)
+    {
+        Path = path;
+        Name = name;
+    }
+
+    public FileDetails(string path, string name, int contentLength, string content) : this(path, name)
+    {
+        ContentLength = contentLength;
+        Content = content;
+    }
+}
